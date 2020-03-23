be65c6a15fbf3bc4a7c5d8aee5ae2219367a8305
Scala Steward <me@scala-steward.org>
2020-03-23 02:03:04 +0100

    Update scaluzzi to 0.1.5

--- sbt-fix/src/main/scala/com/alejandrohdezma/sbt/fix/ScalafixWithDefaultsPlugin.scala
+++ sbt-fix/src/main/scala/com/alejandrohdezma/sbt/fix/ScalafixWithDefaultsPlugin.scala
@@ -82,7 +82,7 @@ object ScalafixWithDefaultsPlugin extends AutoPlugin {
   }
 
   lazy val scalafixDefaultRules: Seq[ModuleID] = Seq(
-    "com.github.vovapolu" %% "scaluzzi"         % "0.1.4.1",
+    "com.github.vovapolu" %% "scaluzzi"         % "0.1.5",
     "com.nequissimus"     %% "sort-imports"     % "0.3.2",
     "com.eed3si9n.fix"    %% "scalafix-noinfer" % "0.1.0-M1"
   )
