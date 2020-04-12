5dc408df925bc412522adc11e087badef8176d9e
Alejandro Hernández (via Travis CI) <info@alejandrohdezma.com>
2020-04-12 13:22:27 +0000

    Upgrade sbt-fix-defaults version

--- sbt-fix/src/main/scala/com/alejandrohdezma/sbt/fix/Defaults.scala
+++ sbt-fix/src/main/scala/com/alejandrohdezma/sbt/fix/Defaults.scala
@@ -23,7 +23,7 @@ object Defaults {
    * Don't change this value. It's automatically updated
    * from `sbt-fix-defaults` releases.
    */
-  private val version = "v0.0.6"
+  private val version = "v0.0.7"
 
   val scalafix: String =
     s"https://github.com/alejandrohdezma/sbt-fix-defaults/releases/download/$version/default.scalafix.conf"
